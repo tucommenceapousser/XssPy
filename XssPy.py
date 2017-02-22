a4aa0d13a6155dec01a1ab1b1a0494f593d100d0
Glenn McGuire <glennmcguire9@gmail.com>
2017-02-22 17:02:57 +1100

    huge code cleanup and removed superfluous try-catches

--- XssPy.py
+++ XssPy.py
@@ -53,78 +53,63 @@ results = parser.parse_args()
 
 logger.setLevel(logging.DEBUG if results.verbose else logging.INFO)
 
-def initializeAndFind(firstDomains):
+def initializeAndFind():
 
-	dummy = 0	#dummy variable for doing nothing
-	firstDomains = []	#list of domains
 	if not results.url:	#if the url has been passed or not
 	    color.log(logging.INFO, color.GREEN, 'Url not provided correctly')
-	    return 0
+	    return
 	
-	smallurl = results.url	#small url is the part of url without http and www
-
+	firstDomains = []	#list of domains
 	allURLS = []
-	allURLS.append(smallurl)	#just one url at the moment
+	allURLS.append(results.url)	#just one url at the moment
 	largeNumberOfUrls = []	#in case one wants to do comprehensive search
 
-        color.log(logging.INFO, color.GREEN, 'Doing a short traversal.')	#doing a short traversal if no command line argument is being passed
-        for url in allURLS:
-                x = str(url)
-                smallurl = x
 
-                try:	# Test HTTPS/HTTP compatibility. Prefers HTTPS but defaults to HTTP if any errors are encountered
-                        test = httplib.HTTPSConnection(smallurl)
-                        test.request("GET", "/")
-                        response = test.getresponse()
-                        if (response.status == 200) | (response.status == 302):
-                                url = "https://www." + str(url)
-                        elif response.status == 301:
-                                loc = response.getheader('Location')
-                                url = loc.scheme + '://' + loc.netloc
-                        else:
-                                url = "http://www." + str(url)
-                except:
-                                url = "http://www." + str(url)
-
-                try:
-                        br.open(url)
-                        color.log(logging.INFO, color.GREEN, 'Finding all the links of the website ' + str(url))
-                        try:
-                                for link in br.links():		#finding the links of the website
-                                        if smallurl in str(link.absolute_url):
-                                                firstDomains.append(str(link.absolute_url))
-                                firstDomains = list(set(firstDomains))
-                        except:
-                                dummy = 0
-                except:
-                        dummy = 0
-        color.log(logging.INFO, color.GREEN, 'Number of links to test are: ' + str(len(firstDomains)))
-		
+	color.log(logging.INFO, color.GREEN, 'Doing a short traversal.')	#doing a short traversal if no command line argument is being passed
+	for url in allURLS:
+		smallurl = str(url)
+		try:	# Test HTTPS/HTTP compatibility. Prefers HTTPS but defaults to HTTP if any errors are encountered
+			test = httplib.HTTPSConnection(smallurl)
+			test.request("GET", "/")
+			response = test.getresponse()
+			if (response.status == 200) | (response.status == 302):
+				url = "https://www." + str(url)
+			elif response.status == 301:
+				loc = response.getheader('Location')
+				url = loc.scheme + '://' + loc.netloc
+			else:
+				url = "http://www." + str(url)
+		except:
+				url = "http://www." + str(url)
+		try:
+			br.open(url)
+			color.log(logging.INFO, color.GREEN, 'Finding all the links of the website ' + str(url))
+			for link in br.links():		#finding the links of the website
+				if smallurl in str(link.absolute_url):
+					firstDomains.append(str(link.absolute_url))
+					firstDomains = list(set(firstDomains))
+		except:
+			pass
+	color.log(logging.INFO, color.GREEN, 'Number of links to test are: ' + str(len(firstDomains)))
 	if results.compOn:
-                color.log(logging.INFO, color.GREEN, 'Doing a comprehensive traversal. This could take a while')
-                for link in firstDomains:
-                        try:
-                                br.open(link)
-                                try:
-                                        for newlink in br.links():	#going deeper into each link and finding its links
-                                                if smallurl in str(newlink.absolute_url):
-                                                        largeNumberOfUrls.append(newlink.absolute_url)
-                                except:
-                                        dummy = 0
-                        except:
-                                dummy = 0
-
-                firstDomains = list(set(firstDomains + largeNumberOfUrls))
-                color.log(logging.INFO, color.GREEN, 'Total Number of links to test have become: ' + str(len(firstDomains)))	#all links have been found
+		color.log(logging.INFO, color.GREEN, 'Doing a comprehensive traversal. This could take a while')
+		for link in firstDomains:
+			try:
+				br.open(link)
+				for newlink in br.links():	#going deeper into each link and finding its links
+					if url in str(newlink.absolute_url):
+						largeNumberOfUrls.append(newlink.absolute_url)
+			except:
+				pass
+		firstDomains = list(set(firstDomains + largeNumberOfUrls))
+		color.log(logging.INFO, color.GREEN, 'Total Number of links to test have become: ' + str(len(firstDomains)))	#all links have been found
 	return firstDomains
 
 
 def findxss(firstDomains):
 	color.log(logging.INFO, color.GREEN, 'Started finding XSS')	#starting finding XSS
 	xssLinks = []			#TOTAL CROSS SITE SCRIPTING FINDINGS
-	count = 0			#to check for forms
-	dummyVar = 0			#dummy variable for doing nothing
-	if len(firstDomains) > 0:	#if there is atleast one link
+	if firstDomains:	#if there is atleast one link
 		for link in firstDomains:
 			y = str(link)
 			color.log(logging.DEBUG, color.YELLOW, str(link))
@@ -133,79 +118,37 @@ def findxss(firstDomains):
 			elif 'pdf' in y:
 				color.log(logging.DEBUG, color.RED, '\tNot a good url to test')
 			else:
-				try:
+				try:	
 					br.open(str(link))	#open the link
-				except:
-					dummyVar = 0
-				try:
-					for form in br.forms():	#check its forms
-						count = count + 1
-				except:
-					dummyVar = 0
-				if count > 0:		#if a form exists, submit it
-					try:
+					if br.forms():		#if a form exists, submit it
 						params = list(br.forms())[0]	#our form
-					except:
-						dummyVar = 0
-					try:
 						br.select_form(nr=0)	#submit the first form
-					except:
-						dummyVar = 0
-					for p in params.controls:
-						par = str(p)
-						if 'TextControl' in par:		#submit only those forms which require text
-                                                    color.log(logging.DEBUG, color.YELLOW, '\tParam: ' + str(p.name))
-                                                    try:
-                                                            br.form[str(p.name)] = '<svg "ons>'		#our payload
-                                                    except:
-                                                            dummyVar = 0
-                                                    try:
-                                                            br.submit()
-                                                    except:
-                                                            dummyVar = 0
-                                                    try:
-                                                            if '<svg "ons>' in br.response().read():	#if payload is found in response, we have XSS
-                                                                    color.log(logging.INFO, color.BOLD+color.GREEN, 'Xss found and the link is ' + str(link) + ' And the payload is <svg \"ons>')
-                                                                    xssLinks.append(link)
-                                                            else:
-                                                                    dummyVar = 0
-                                                    except:
-                                                            color.log(logging.INFO, color.RED, '\tcould not read the page')
-                                                    try:
-                                                            br.back()
-                                                    except:
-                                                            dummyVar = 0
-
-                                                    #SECOND PAYLOAD
-
-                                                    try:
-                                                            br.form[str(p.name)] = 'javascript:alert(1)'	#second payload
-                                                    except:
-                                                            dummyVar = 0
-                                                    try:
-                                                            br.submit()
-                                                    except:
-                                                            dummyVar = 0
-                                                    try:
-                                                            if '<a href="javascript:alert(1)' in br.response().read():
-                                                                    color.log(logging.INFO, 'Xss found and the link is ' + str(link) + ' And the payload is javascript:alert(1)')
-                                                                    xssLinks.append(link)
-                                                            else:
-                                                                    dummyVar = 0
-                                                    except:
-                                                            color.log(logging.INFO, color.RED, '\tCould not read a page')
-                                                    try:
-                                                            br.back()		#go back to the previous page
-                                                    except:
-                                                            dummyVar = 0
-                                                count = 0
-                color.log(logging.DEBUG, color.GREEN+color.BOLD, 'The following links are vulnerable: ')
-                for link in xssLinks:		#print all xss findings
+						for p in params.controls:
+							par = str(p)
+							if 'TextControl' in par:		#submit only those forms which require text
+								color.log(logging.DEBUG, color.YELLOW, '\tParam: ' + str(p.name))
+								br.form[str(p.name)] = '<svg "ons>'		#our payload
+								br.submit()
+								if '<svg "ons>' in br.response().read():	#if payload is found in response, we have XSS
+									color.log(logging.INFO, color.BOLD+color.GREEN, 'Xss found and the link is ' + str(link) + ' And the payload is <svg \"ons>')
+									xssLinks.append(link)
+								br.back()
+								#SECOND PAYLOAD
+								br.form[str(p.name)] = 'javascript:alert(1)'	#second payload
+								br.submit()
+								if '<a href="javascript:alert(1)' in br.response().read():
+									color.log(logging.INFO, 'Xss found and the link is ' + str(link) + ' And the payload is javascript:alert(1)')
+									xssLinks.append(link)
+									br.back()
+						count = 0
+				except:
+					pass
+		color.log(logging.DEBUG, color.GREEN+color.BOLD, 'The following links are vulnerable: ')
+		for link in xssLinks:		#print all xss findings
 			color.log(logging.DEBUG, color.GREEN, '\t'+link)
 	else:
 		color.log(logging.INFO, color.RED+color.BOLD, '\tNo link found, exiting')
 
 #calling the function
-firstDomains = []
-firstDomains = initializeAndFind(firstDomains)
+firstDomains = initializeAndFind()
 findxss(firstDomains)
